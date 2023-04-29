afff68032039f4b6829723506a2884c0c686e8dc
Kevin Gomes <kevinmg50@gmail.com>
2023-04-29 15:03:25 -0300

    fix: corrigido erro de os produtos não serem gravados em json

--- src/main/java/mechanicops/controller/GerenciadorProdutos.java
+++ src/main/java/mechanicops/controller/GerenciadorProdutos.java
@@ -1,30 +1,32 @@
 package mechanicops.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import mechanicops.model.Produto;
 
-import java.io.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedWriter;
+
 
 public class GerenciadorProdutos {
 
-    private static final String ARQUIVO_PRODUTOS = "src/main/java/mechanicops/database/produtos.txt";
+    private static final String ARQUIVO_PRODUTOS = "src/main/java/mechanicops/database/produtos.json";
+
     public GerenciadorProdutos() {
-        File file = new File(ARQUIVO_PRODUTOS);
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                System.err.println("Erro ao criar arquivo de produtos: " + e.getMessage());
-            }
-        }
     }
 
     public void adicionarProduto(Produto produto) {
         try {
+            Gson gson = new Gson();
+            String produtoJson = gson.toJson(produto);
+
             FileWriter fileWriter = new FileWriter(ARQUIVO_PRODUTOS, true);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(produto.toString());
+            bufferedWriter.write(produtoJson);
             bufferedWriter.newLine();
             bufferedWriter.close();
             fileWriter.close();
@@ -48,26 +50,30 @@ public class GerenciadorProdutos {
         List<Produto> produtos = new ArrayList<>();
         try {
             FileReader fileReader = new FileReader(ARQUIVO_PRODUTOS);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String linha;
+            JsonReader jsonReader = new JsonReader(fileReader);
+            Gson gson = new Gson();
 
-            while ((linha = bufferedReader.readLine()) != null) {
-                String[] partes = linha.split(",");
-                int id = Integer.parseInt(partes[0]);
-                String nome = partes[1];
-                String descricao = partes[2];
-                float valor = Float.parseFloat(partes[3]);
-                int quantidade = Integer.parseInt(partes[4]);
-
-                Produto produto = new Produto(id, nome, descricao, valor, quantidade);
+            jsonReader.beginArray();
+            while (jsonReader.hasNext()) {
+                Produto produto = gson.fromJson(jsonReader, Produto.class);
                 produtos.add(produto);
             }
+            jsonReader.endArray();
 
-            bufferedReader.close();
+            jsonReader.close();
             fileReader.close();
         } catch (IOException e) {
             System.err.println("Erro ao ler produtos: " + e.getMessage());
         }
         return produtos;
+    }
+
+    public String listarProdutosFormatado() {
+        StringBuilder sb = new StringBuilder();
+        List<Produto> produtos = listarProdutos();
+        for (Produto produto : produtos) {
+            sb.append(produto.toString()).append("\n");
+        }
+        return sb.toString();
     }
 }

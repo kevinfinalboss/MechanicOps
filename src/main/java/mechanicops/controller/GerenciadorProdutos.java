96e894012c599a9fcf1fda16eb0e0dd8cce5a58f
Kevin Gomes <kevinmg50@gmail.com>
2023-04-29 13:29:26 -0300

    fix: alterado o path para salvar os produtos

--- src/main/java/mechanicops/controller/GerenciadorProdutos.java
+++ src/main/java/mechanicops/controller/GerenciadorProdutos.java
@@ -1,7 +1,6 @@
 package mechanicops.controller;
 
 import mechanicops.model.Produto;
-import org.json.JSONObject;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -9,8 +8,7 @@ import java.util.List;
 
 public class GerenciadorProdutos {
 
-    private static final String ARQUIVO_PRODUTOS = "src/mechanicops/database/produtos.txt";
-
+    private static final String ARQUIVO_PRODUTOS = "src/main/java/mechanicops/database/produtos.txt";
     public GerenciadorProdutos() {
         File file = new File(ARQUIVO_PRODUTOS);
         if (!file.exists()) {
@@ -54,12 +52,12 @@ public class GerenciadorProdutos {
             String linha;
 
             while ((linha = bufferedReader.readLine()) != null) {
-                JSONObject json = new JSONObject(linha);
-                int id = json.getInt("id");
-                String nome = json.getString("nome");
-                String descricao = json.getString("descricao");
-                float valor = (float) json.getDouble("valor");
-                int quantidade = json.getInt("quantidade");
+                String[] partes = linha.split(",");
+                int id = Integer.parseInt(partes[0]);
+                String nome = partes[1];
+                String descricao = partes[2];
+                float valor = Float.parseFloat(partes[3]);
+                int quantidade = Integer.parseInt(partes[4]);
 
                 Produto produto = new Produto(id, nome, descricao, valor, quantidade);
                 produtos.add(produto);

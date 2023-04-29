4bb8f8823e9938898d46fb4555e2c8680c3b95ff
Kevin Gomes <kevinmg50@gmail.com>
2023-04-29 12:33:00 -0300

    feat: refatorado para estrutura mvc

---
+++ src/mechanicops/Main.java
@@ -0,0 +1,10 @@
+package mechanicops;
+
+import mechanicops.view.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.exibir();
+    }
+}
